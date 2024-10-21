0c1b46149b4ecb71e89fb93656f42a87d309455b
Subhrajitsarkar <subhrajitsarkar28@gmail.com>
2024-10-22 01:49:30 +0530

    i just started the express framework
    learned about express basic, middleware, next function, res.send function

---
+++ 1started_with_express/app.js
@@ -0,0 +1,61 @@
+// let http = require('http');
+// const express = require('express')
+// const app = express()
+
+// let server = http.createServer(app)
+
+// server.listen(3000)
+
+
+
+
+// let http = require('http');
+// const express = require('express')
+// const app = express()
+
+// app.use((req, res, next) => {
+//     console.log('middleware');
+//     next() //allows the req to continue to the next middleware
+// })
+// app.use((req, res, next) => {
+//     console.log('2nd middleware');
+
+// })
+// let server = http.createServer(app)
+
+// server.listen(3000)
+
+
+
+
+// let http = require('http');
+// const express = require('express')
+// const app = express()
+
+// app.use((req, res, next) => {
+//     console.log('middleware');
+//     next()
+// })
+// app.use((req, res, next) => {
+//     console.log('2nd middleware');
+//     res.send('<h1>hello from express</h1>')
+// })
+// let server = http.createServer(app)
+
+// server.listen(3000)
+
+
+
+
+const express = require('express')
+const app = express()
+
+app.use((req, res, next) => {
+    console.log('middleware');
+    next()
+})
+app.use((req, res, next) => {
+    console.log('2nd middleware');
+    res.send('<h1>hello from express</h1>')
+})
+app.listen(3000)

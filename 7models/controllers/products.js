0f414749298174402bbd798679f0a0d72b1018de
Subhrajitsarkar <subhrajitsarkar28@gmail.com>
2024-11-02 19:01:21 +0530

    change done
    i used // for better understanding

--- 7models/controllers/products.js
+++ 7models/controllers/products.js
@@ -16,15 +16,21 @@ exports.postAddProduct = (req, res, next) => {
   res.redirect('/');
 };
 
+// Exporting the getProducts function to use in other parts of the application (like routes)
 exports.getProducts = (req, res, next) => {
+
+  // Fetch all products from the Product model
   Product.fetchAll(products => {
+
+    // Render the 'shop' view and pass data to it
     res.render('shop', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+      prods: products,               // List of products to display
+      pageTitle: 'Shop',             // Title of the page
+      path: '/',                     // Current path for navigation or highlighting in templates
+      hasProducts: products.length > 0,  // Boolean to check if there are products to show
+      activeShop: true,              // Activates specific CSS styling for the shop view (optional)
+      productCSS: true               // Additional styling for product-related elements (optional)
     });
   });
 };
+
